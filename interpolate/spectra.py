c7b9e1823ab294837c79310c777a0505cc556cc5
Henrique Miranda <miranda.henrique@gmail.com>
2018-01-06 10:39:44 +0000

    Added script to generate the vibrational spectra.

--- interpolate/spectra.py
+++ interpolate/spectra.py
@@ -6,12 +6,15 @@ from phonopy.structure.atoms import Atoms
 import phonopy.file_IO as file_IO
 import matplotlib.pyplot as plt
 
+#tag=sys.argv[1]
+tag='masses'
 options = {'all':    {'forces':True, 'masses':True },
            'masses': {'forces':False,'masses':True },
            'forces': {'forces':True, 'masses':False}}
 emin=50
 emax=450
 energies_cm1 = np.linspace(emin,emax,500)
+steps = [0, 0.24, 0.26, 0.43, 0.47, 0.63, 0.70, 0.80, 0.90, 1.00]
 
 def get_dos(x,freqs,sigma=3):
     y = np.zeros(len(x))
@@ -49,6 +52,7 @@ def init_phonon(atoms, supercell_matrix, force_constants):
     #create phonon structure
     phonon = Phonopy(atoms,supercell_matrix)
     phonon.set_force_constants(force_constants)
+    phonon.symmetrize_force_constants_by_space_group()
 
     return phonon
 
@@ -124,12 +128,11 @@ def mix_phonon(struct1,struct2,ratio1,forces=True,cell=True,masses=True):
 cuges = read_ingredients('cuges')
 cusns = read_ingredients('cusns')
 
-steps = np.linspace(0,1,10)
 
-def calculate_freqs(tag, s1=cusns, s2=cuges):
+def calculate_freqs(tag):
     phfreqs = []
     for x in steps:
-        phonon = mix_phonon(s1, s2, x, **options[tag])
+        phonon = mix_phonon(cusns, cuges, x, **options[tag])
         freqs = phonon.get_frequencies((0,0,0))*THzToCm
         phfreqs.append(freqs)
         print("%5.3lf %12.8lf" % (x, freqs[-1]))
@@ -163,45 +166,33 @@ def plot_frequencies(phfreqs,ax=plt,save=False):
 
     wmin = phfreqs[-1,-3]
     wmax = phfreqs[ 0,-3]
-    ax.axhline(wmin,ls='--',lw=1,c='r')
-    ax.axhline(wmax,ls='--',lw=1,c='r')
-    ax.text(.01,wmax+2,"%6.2lf"%wmax)
-    ax.text(.78,wmin+2,"%6.2lf"%wmin)
+    ax.axhline(wmin,ls='--',lw=1)
+    ax.axhline(wmax,ls='--',lw=1)
+    ax.text(-.20,wmax,"%6.2lf"%wmax)
+    ax.text(1.01,wmin,"%6.2lf"%wmin)
     print(wmax-wmin)
 
     for phfreq in phfreqs:
-        ax.plot(steps,phfreqs,c='C0',alpha=0.3)
+        ax.plot(steps,phfreqs,c='C0')
 
     ax.set_xlim(min(steps),max(steps))
     ax.set_ylim(200,450)
     ax.set_xlabel('x')
     if save: plt.savefig('cuges_hse06_%s_freqs.pdf'%tag)
     return ax
+   
+size=4 
+fig = plt.figure(figsize=(size,size))
+fig.suptitle("CuGeS to Cu(Sn$_{1-x}$Ge$_x$)S HSE06")
 
-size=3   
-fig = plt.figure(figsize=(size*3,size+1.5))
-#fig.suptitle("CuGeS to Cu(Sn$_{1-x}$Ge$_x$)S HSE06")
-
-ax = plt.subplot(1,3,1)
-ax.set_title('a) interpolation of forces\n and masses')
-ax.set_ylabel('frequencies cm$^{-1}$')
+ax = plt.subplot(1,1,1)
+ax.set_title('all')
 phfreqs = calculate_freqs('all')
-plot_frequencies(phfreqs,ax)
 
-ax = plt.subplot(1,3,2)
-ax.set_title('b) interpolation of forces only\n using the masses of CTS')
-#ax.set_ylabel('frequencies cm$^{-1}$')
-#ax.get_yaxis().set_visible(False)
-phfreqs = calculate_freqs('forces')
-plot_frequencies(phfreqs,ax)
+#save text files with the data
+for i,s in enumerate(steps):
+    e,dos = get_dos(energies_cm1,phfreqs[i])
+    data = np.array([e,dos]).T
+    np.savetxt('raman_x_%.2lf.dat'%s,data,fmt='%8.4e')
 
-ax = plt.subplot(1,3,3)
-ax.set_title('c) interpolation of masses only\n using the forces of CGS')
-#ax.set_ylabel('frequencies cm$^{-1}$')
-#ax.get_yaxis().set_visible(False)
-phfreqs = calculate_freqs('masses', s1=cuges, s2=cusns)
-phfreqs.reverse()
-plot_frequencies(phfreqs,ax)
-plt.subplots_adjust(left=0.1, right=0.95,wspace=0.2,bottom=0.15,top=0.86)
-plt.savefig('freqs_3colum.pdf')
-plt.show()
+
